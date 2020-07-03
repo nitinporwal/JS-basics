29686008ebdf5365700aa5ef0e9ef977bb641a60
nitin-codr <nitinporwal99@gmail.com>
2020-07-03 22:22:33 +0530

    variables and their data types

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+// JS is a dynamic type language where data types are automatically assigned to variables
+
+let firstName = 'John'; // You can also use double quotes for string like "John" but using single quotes is a bit modern
+console.log(firstName);
+
+let lastName = 'Smith';
+let age = 32; // Floating point numbers by default (both integer and decimals)
+
+let fullAge = true; // Boolean data type true/false
+console.log(fullAge);
+
+let job; // undefined data type
+console.log(job);
+
+job = 'Doctor';
+console.log(job);
+
+let noData = null; // null data type
+console.log(noData);
+
+// Rules for naming variables:
+
+// let 10bucks = 10; // can't use number at the start of variable names
+
+// let me&you = 'Me and you'; // can't use any characters except _ and $ in variable names
+
+let _10bucks = 10; // correct
+
+// let if = 'else' // can't use reserved js keywords as variable names
+
