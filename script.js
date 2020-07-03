2cc8c3c79a233a24c98eb3fc0a221b931632f080
nitin-codr <nitinporwal99@gmail.com>
2020-07-03 22:41:42 +0530

    Type Coercion and Variable mutation

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // JS is a dynamic type language where data types are automatically assigned to variables
 
+/**************************
+ * Variables and data types
+ **************************/
+
 let firstName = 'John'; // You can also use double quotes for string like "John" but using single quotes is a bit modern
 console.log(firstName);
 
@@ -20,11 +24,41 @@ console.log(noData);
 
 // Rules for naming variables:
 
-// let 10bucks = 10; // can't use number at the start of variable names
+/*
 
-// let me&you = 'Me and you'; // can't use any characters except _ and $ in variable names
+let 10bucks = 10; // can't use number at the start of variable names
 
 let _10bucks = 10; // correct
 
-// let if = 'else' // can't use reserved js keywords as variable names
+let me&you = 'Me and you'; // can't use any characters except _ and $ in variable names
 
+let if = 'else' // can't use reserved js keywords as variable names
+
+*/
+
+/******************************
+ * Mutation and Type Coercion
+ *******************************/
+
+ let firstName = 'John';
+ let age = 32;
+
+ // Types Coercion
+ console.log(firstName + ' ' + age); // -> John 32 in console, js by itself converts number into string
+
+ let job, isMarried, marry; // initially undefined
+ job = 'Doctor';
+ isMarried = true;
+
+ console.log(firstName + ' is a ' + age + ' year old ' + job + '. ' + marry + ' Is he married? ' + isMarried); // by using type coercion js converts undefined and boolean to string
+
+// Variable Mutation
+
+age = 'thirty two'; // number age is mutated to string age
+
+job = 'plumber';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. ' + marry + ' Is he married? ' + isMarried);
+
+let lastName = prompt('What is your last name?'); // Prompting user to type last name
+console.log(firstName + ' ' + lastName);
